c9c2040c615f1d15c3ac4028da8c5f447bec11f1
JakeSiewJK64 <joekanesiew@outlook.com>
2022-07-18 16:53:25 +0800

    update: added cross origin annotation for expense controller.

--- src/main/java/com/jakesiewjk64/budgetlab/controllers/ExpenditureController.java
+++ src/main/java/com/jakesiewjk64/budgetlab/controllers/ExpenditureController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,13 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jakesiewjk64.budgetlab.models.ExpenseModel;
 import com.jakesiewjk64.budgetlab.services.ExpenseService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/expense")
 public class ExpenditureController {
 
 	@Autowired
 	private ExpenseService expenseService;
-	
+
 	@GetMapping("/getExpenseById/{id}")
 	public Optional<ExpenseModel> getExpenseById(@PathVariable long id) {
 		return expenseService.getOneExpense(id);
