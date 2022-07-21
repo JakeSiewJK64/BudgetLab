294e758ea2a06206196cc1124e56af53d73e8665
JakeSiewJK64 <joekanesiew@outlook.com>
2022-07-21 14:15:37 +0800

    update: removed unused declarations in auth guard

--- clientapp/src/app/services/auth-guard.service.ts
+++ clientapp/src/app/services/auth-guard.service.ts
@@ -1,10 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  Router,
-  RouterStateSnapshot,
-} from '@angular/router';
-import { AuthenticationService } from './authentication.service';
+import { Router } from '@angular/router';
 import { PostAuthenticateService } from './ResultsService/post-authenticate-result.service';
 
 @Injectable({
@@ -12,15 +7,11 @@ import { PostAuthenticateService } from './ResultsService/post-authenticate-resu
 })
 export class AuthGuardService {
   constructor(
-    private authService: AuthenticationService,
     private router: Router,
     private postAuth: PostAuthenticateService
   ) {}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean {
+  canActivate(): boolean {
     let jwtToken = localStorage.getItem('token');
     if (jwtToken == null || jwtToken == undefined || jwtToken.length == 0) {
       this.postAuth.emitLoggedIn();
