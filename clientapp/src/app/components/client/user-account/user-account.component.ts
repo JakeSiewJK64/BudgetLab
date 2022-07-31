f0f515c4156b581f04f3cdfb984b87b6686b8d36
DESKTOP-TN9N4CC\Think <joekanesiew@outlook.com>
2022-07-31 11:55:52 +0800

    update: added dialog to confirm user update action

--- clientapp/src/app/components/client/user-account/user-account.component.ts
+++ clientapp/src/app/components/client/user-account/user-account.component.ts
@@ -1,7 +1,9 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { UserModel } from 'src/app/models/UserModel';
 import { AuthenticationService } from 'src/app/services/authentication.service';
 import { UserService } from 'src/app/services/user.service';
+import { AlertdialogComponent } from '../../shared/alertdialog/alertdialog.component';
 
 @Component({
   selector: 'app-user-account',
@@ -9,16 +11,29 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./user-account.component.scss'],
 })
 export class UserAccountComponent implements AfterViewInit {
-  constructor(private authService: AuthenticationService,
+  constructor(
+    private authService: AuthenticationService,
+    private customDialog: MatDialog,
     private userService: UserService) { }
 
   user: UserModel;
   file: string | ArrayBuffer | null | undefined;
 
   save() {
-    this.userService.upsertUsers(this.user).subscribe({
-      next: (x) => {
-        console.log(x);
+    this.customDialog.open(AlertdialogComponent, {
+      data: {
+        title: "Update Profile",
+        message: "Are you sure you want to update your profile? Updating means logging out of your account to apply the changes you made, especially your username."
+      }
+    }).afterClosed().subscribe({
+      next: x => {
+        if (x == 'confirm') {
+          this.userService.upsertUsers(this.user).subscribe({
+            next: (res) => {
+              console.log(res);
+            }
+          })
+        }
       }
     })
   }
